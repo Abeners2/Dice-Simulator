4282fc1ae0aefc5389a11d487b6bd19eff7601ce
Abeners2 <abnerlucassantoss2@gmail.com>
2024-06-17 23:29:59 -0300

    Att Calculadora sim.py

--- simulador_de_dados.py
+++ simulador_de_dados.py
@@ -1,4 +1,7 @@
+# Arquivo sim_dados.py na raiz do projeto 'simulador_de_dados'
+
 import random
+from dados.calculadora import calcular_media  # Importa a função calcular_media do módulo calculadora
 
 def simular_lancamento_dados(numero_de_lancamentos):
     resultados = []
@@ -16,6 +19,9 @@ def main():
     print("\nResultados dos lançamentos:")
     for i, resultado in enumerate(resultados, start=1):
         print(f"Lançamento {i}: {resultado}")
+    
+    media = calcular_media(resultados)
+    print(f"\nMédia dos resultados: {media:.2f}")
 
 if __name__ == "__main__":
     main()
