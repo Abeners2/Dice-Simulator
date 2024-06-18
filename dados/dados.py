66fcd2a604810c9618646ab2da54b5032b068cef
Abeners2 <abnerlucassantoss2@gmail.com>
2024-06-18 05:51:56 -0300

    Att dados.py

--- dados/dados.py
+++ dados/dados.py
@@ -2,9 +2,9 @@
 
 import random
 
-def simular_lancamento_dados(numero_de_lancamentos):
+def simular_lancamento_dados(numero_de_lancamentos, faces):
     resultados = []
     for _ in range(numero_de_lancamentos):
-        resultado = random.randint(1, 6)  # Gera um número aleatório entre 1 e 6 (faces de um dado)
+        resultado = random.randint(1, faces)  # Gera um número aleatório entre 1 e o número de faces do dado
         resultados.append(resultado)
     return resultados
